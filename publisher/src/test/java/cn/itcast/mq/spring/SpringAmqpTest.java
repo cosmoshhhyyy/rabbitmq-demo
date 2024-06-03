da0c9da5f05baac37f69b339c188d03011c09299
lpy <2552696563@qq,com>
2024-06-03 22:59:57 +0800

    rabbitmq完结

--- publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringAmqpTest {
@@ -44,5 +47,50 @@ public class SpringAmqpTest {
         // 消息
         String message = "hello fanout!";
         rabbitTemplate.convertAndSend(exchangeName, "", message);
+    }
+
+    @Test
+    public void testSendDirectExchange() {
+        // 交换机名称
+        String exchangeName = "lpy.direct";
+        // 消息
+        String message = "hello direct red";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "red", message);
+
+        // 消息
+        String message2 = "hello direct blue";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "blue", message2);
+
+        // 消息
+        String message3 = "hello direct yellow";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "yellow", message3);
+    }
+
+    @Test
+    public void testSendTopicExchange() {
+        // 交换机名称
+        String exchangeName = "lpy.topic";
+        // 消息
+        String message = "hello topic a.b";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "a.b", message);
+
+        // 消息
+        String message1 = "hello topic a.b.c";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "a.b.c", message1);
+    }
+
+    @Test
+    public void testSendMap() throws InterruptedException {
+        // 准备消息
+        Map<String,Object> msg = new HashMap<>();
+        msg.put("name", "小宇");
+        msg.put("age", 18);
+        // 发送消息
+        rabbitTemplate.convertAndSend("simple.queue", msg);
     }
 }
